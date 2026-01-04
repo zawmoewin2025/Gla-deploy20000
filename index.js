61c1acaa68daf9dc0acddccab112d26fde39ee96
zawmoewin120298-collab <zawmoewin120298@gmail.com>
2026-01-04 11:17:49 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const UUID = process.env.UUID || '84705c0d-5036-44b1-a07e-d1582e653595'; // 使�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'www.speedtest.net.cdn.cloudflare.net.speedtestnet.ggff.net';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNTBlNjY3NDA4YTBjMWQ1MmVmNTBhZmIyNGViNmViOGEiLCJ0IjoiZWEwMWY1MzAtMDBjNy00MTAxLWFlM2EtNjNhYmY1MGZlMmRhIiwicyI6Ik1qZ3lOV1ptWm1VdE16QTBOaTAwWWpaaExXSXlPVEl0WXpjeFlUWmpZV00xTW1NMiJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
@@ -625,3 +625,4 @@ app.get("/", async function(req, res) {
 });
 
 app.listen(PORT, () => console.log(`http server is running on port:${PORT}!`));
+
